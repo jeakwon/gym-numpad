92a5a3cdaa7c89371cc626a48539fb6726e59a93
jeakwon <33548421+jeakwon@users.noreply.github.com>
2022-09-01 23:48:02 +0900

    Update numpad_env.py

--- gym_numpad/envs/numpad_env.py
+++ gym_numpad/envs/numpad_env.py
@@ -13,7 +13,7 @@ from gym.utils.renderer import Renderer
 class NumPadEnv(gym.Env):
     metadata = {
         "render_modes": ["human", "rgb_array", "single_rgb_array"],
-        "render_fps": 30,
+        "render_fps": 50,
     }
     def __init__(self, render_mode: Optional[str] = None, size=2, cues=['a', 'b', 'c', 'd', 'e'], init_pos=[0, 0]):
         self.size = 2*size+1
@@ -34,7 +34,7 @@ class NumPadEnv(gym.Env):
         self.render_mode = render_mode
         self.renderer = Renderer(self.render_mode, self._render)
 
-        self.screen_dim = 500
+        self.screen_dim = 600
         self.screen = None
         self.clock = None
         self.isopen = True
@@ -108,42 +108,41 @@ class NumPadEnv(gym.Env):
             self.clock = pygame.time.Clock()
         
         self.surf = pygame.Surface((self.screen_dim, self.screen_dim))
-        self.surf.fill((255, 255, 255))
+        self.surf.fill((100, 100, 100))
         
         L = self.screen_dim/self.size
 
         # show init position 
-        x = int(self._init_pos[0] * L + 0.5 + 1)
-        y = int(self._init_pos[1] * L + 0.5 + 1)
-        w = int(L + 0.5 - 1)
-        h = int(L + 0.5 - 1)
-        pygame.draw.rect(self.surf, [128, 128, 128], [x, y, w, h])
+        x = int(self._init_pos[0]*L+1)
+        y = int(self._init_pos[1]*L+1)
+        w = int(L)
+        h = int(L)
+        pygame.draw.rect(self.surf, [140, 140, 140], [x, y, w, h])
+
+        # show rewards
+        for i, reward_seq in enumerate(self.reward_seqs):
+            x = int(reward_seq[0]*L+1)
+            y = int(reward_seq[1]*L+1)
+            w = int(L)
+            h = int(L)
+            reward_pos = [x, y, w, h]
+            pygame.draw.rect(self.surf, [35, 35, 35], reward_pos)
 
         # show grid lines
         for i in range(self.size+1):
-            pygame.draw.line(self.surf, [0 ,0, 0], (0, i*L), (self.screen_dim, i*L))
-            pygame.draw.line(self.surf, [0 ,0, 0], (i*L, 0), (i*L, self.screen_dim))
-
-        # show agent
-        for i, reward_seq in enumerate(self.reward_seqs):
-            x = int(reward_seq[0] * L + 0.5 + 1)
-            y = int(reward_seq[1] * L + 0.5 + 1)
-            w = int(L + 0.5 - 1)
-            h = int(L + 0.5 - 1)
-            reward_pos = [x, y, w, h]
-            color = [0, 255, 0] if i==0 else [255, 0, 0]
-            pygame.draw.rect(self.surf, color, reward_pos)
+            pygame.draw.line(self.surf, [255 ,255, 255], (0, i*L), (self.screen_dim, i*L))
+            pygame.draw.line(self.surf, [255 ,255, 255], (i*L, 0), (i*L, self.screen_dim))
             
+        # show agent
         agent_pos = L*(np.array(self.pos)+0.5)
-        pygame.draw.circle(self.surf, [0, 0, 0, 0], agent_pos, L/2)
-
+        pygame.draw.circle(self.surf, [243, 135, 47], agent_pos, L/4)
         self.screen.blit(self.surf, (0, 0))
         
         # show cues
         for i in range(self.size):
             for j in range(self.size):
-                font = pygame.font.SysFont("arial", 24) 
-                text = font.render(self.cues[self.Q[i, j]], False, [0, 0, 0])
+                font = pygame.font.SysFont("arial", int(L/3), bold=False)
+                text = font.render(' '+self.cues[self.Q[i, j]], False, [255, 255, 255])
                 self.screen.blit(text, [i*L, j*L])
 
         # show reward sequence
@@ -151,8 +150,9 @@ class NumPadEnv(gym.Env):
             R_tot = len(self.reward_zones)
             R_rem = len(self.reward_seqs)
             K = np.arange(R_tot)[-R_rem:][k]+1
-            font = pygame.font.SysFont("arial", 30) 
-            text = font.render(str(K), False, [0, 0, 0])
+            font = pygame.font.SysFont("arial", int(L/2), bold=True)
+            color = [243, 135, 47] if [i, j] == self.reward_seqs[0] else [45, 45, 45]
+            text = font.render(f'{K}', False, color)
             zone = text.get_rect(center=[(i+0.5)*L, (j+0.5)*L])
             self.screen.blit(text, zone)
         
@@ -174,12 +174,20 @@ class NumPadEnv(gym.Env):
             pygame.quit()
             self.isopen = False
 
-class NumPadEnv2x2(NumPadEnv):
-    def __init__(self, render_mode='human', cues=[1, 2, 3, 4, 5]):
-        super(NumPadEnv2x2, self).__init__(render_mode=render_mode, size=5)
+class NumPad2x2(NumPadEnv):
+    def __init__(self, render_mode=None):
+        super().__init__(render_mode=render_mode, size=2, cues=['a', 'b', 'c', 'd'], init_pos=None)
+
+class NumPad3x3(NumPadEnv):
+    def __init__(self, render_mode=None):
+        super().__init__(render_mode=render_mode, size=3, cues=['a', 'b', 'c', 'd', 'e', 'f'], init_pos=None)
+
+class NumPad4x4(NumPadEnv):
+    def __init__(self, render_mode=None):
+        super().__init__(render_mode=render_mode, size=4, cues=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'], init_pos=None)
 
 if __name__ == "__main__":
-    env = NumPadEnv(render_mode='human', size=7, init_pos=None)
+    env = NumPad2x2('human')
     for episode in range(30):
         env.reset(seed=episode)
         env.action_space.seed(episode)
