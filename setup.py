abffe4198c2a621422d0affc8ceec6269fbb54de
jeakwon <33548421+jeakwon@users.noreply.github.com>
2022-09-01 23:52:10 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="gym_numpad",
+      version="0.1",
+      url="https://github.com/jeakwon/gym-numpad",
+      author="Jea Kwon",
+      license="MIT",
+      packages=["gym_numpad", "gym_numpad.envs"],
+      install_requires = ["gym", "pygame", "numpy"]
+)
