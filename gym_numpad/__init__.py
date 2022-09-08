f414a3bc2748320a4e7e6bda1781452afedb47fb
jeakwon <33548421+jeakwon@users.noreply.github.com>
2022-09-08 11:05:15 +0900

    Update __init__.py

--- gym_numpad/__init__.py
+++ gym_numpad/__init__.py
@@ -4,3 +4,21 @@ register(
     id='numpad2x2_test-v0',
     entry_point='gym_numpad.envs:NumPad2x2_test',
 )
+
+
+register(
+    id='numpad2x2-v0',
+    entry_point='gym_numpad.envs:NumPad2x2',
+)
+
+
+register(
+    id='numpad3x3-v0',
+    entry_point='gym_numpad.envs:NumPad3x3',
+)
+
+
+register(
+    id='numpad4x4-v0',
+    entry_point='gym_numpad.envs:NumPad4x4',
+)
