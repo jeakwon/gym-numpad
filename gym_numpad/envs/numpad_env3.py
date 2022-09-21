706e1d9d61cd0e81ff691e5c4609af4c77b004fe
jeakwon <33548421+jeakwon@users.noreply.github.com>
2022-09-21 10:42:05 +0900

    Update numpad_env3.py

--- gym_numpad/envs/numpad_env3.py
+++ gym_numpad/envs/numpad_env3.py
@@ -56,7 +56,9 @@ class NumPadEnv(gym.Env):
         non_reward_positions = np.argwhere(self.map[2] == 0)
         i, j = random.choice(non_reward_positions)
         
-        self.expected_max_rwd = self.steps_per_episode/rwd_seq_1cycle_dist(self.map[2])
+        possible_rwd_cycles = (self.steps_per_episode - 1) / rwd_seq_1cycle_dist(self.map[3])
+        rwds_per_cycle = self.map[2].sum()
+        self.expected_max_rwd = possible_rwd_cycles * rwds_per_cycle
 
         self.state = deepcopy(self.map)
         self.state[0][i, j] = 1  # set agent init position
